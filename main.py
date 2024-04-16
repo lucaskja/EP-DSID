34bf14b4d2686aec368c2b30d9f78de8d0bceefa
lucaskja <lucaskja@usp.br>
2024-04-16 01:55:23 -0300

    Refactor Peer class to remove unused code and improve code organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
-from peer import Peer
-
+import sys
+from random import choice
+from time import sleep
+from servidor_tcp import ServidorTCP
 
 class Main:
     def __init__(self, endereco, porta, vizinhos=None, lista_chave_valor=None):
@@ -62,6 +64,7 @@ class Main:
             linhas = file.readlines()
             for linha in linhas:
                 chave, valor = linha.strip().split(" ")
+                print(f"Adicionando par ({chave}, {valor}) na tabela local")
                 lista_chave_valor[chave] = valor
         return lista_chave_valor
 
@@ -79,6 +82,45 @@ class Main:
         vizinho = self.vizinhos[int(input_vizinho)]
         mensagem = f"{peer.endereco}:{peer.porta} {peer.sequencia} {peer.ttl} HELLO"
         peer.enviar_mensagem(vizinho, mensagem)
+        
+    def iniciar_flooding(self, peer):
+        # Função para enviar uma mensagem SEARCH (flooding)
+        input_chave = input("Digite a chave a ser buscada\n")
+        ip_origem = f"{peer.endereco}:{peer.porta}"
+        mensagem = f"{ip_origem} {peer.sequencia} {peer.ttl} SEARCH FL {peer.porta} {input_chave} 1"
+        peer.mensagens_vistas.add((ip_origem, peer.sequencia))
+        for vizinho in peer.vizinhos:
+            peer.enviar_mensagem(vizinho, mensagem)
+    
+    def iniciar_random_walk(self, peer):
+        # Função para enviar uma mensagem SEARCH (random walk)
+        input_chave = input("Digite a chave a ser buscada\n")
+        ip_origem = f"{peer.endereco}:{peer.porta}"
+        mensagem = f"{ip_origem} {peer.sequencia} {peer.ttl} SEARCH RW {peer.porta} {input_chave} 1"
+        vizinho_escolhido = choice(peer.vizinhos)
+        peer.enviar_mensagem(vizinho_escolhido, mensagem)
+    
+    def iniciar_busca_profundidade(self, peer):
+        # Função para enviar uma mensagem SEARCH (busca em profundidade)
+        input_chave = input("Digite a chave a ser buscada\n")
+        noh_mae = f"{peer.endereco}:{peer.porta}"
+        mensagem = f"{noh_mae} {peer.sequencia} {peer.ttl} SEARCH BP {peer.porta} {input_chave} 1"
+        vizinhos_candidatos = peer.vizinhos.copy()
+        proximo_vizinho = choice(vizinhos_candidatos)
+        vizinho_ativo = proximo_vizinho
+        vizinhos_candidatos.remove(proximo_vizinho)
+        peer.enviar_mensagem(proximo_vizinho, mensagem)
+    
+    def estatisticas(self, peer):
+        # Função para exibir estatísticas
+        pass
+    
+    def bye(self, peer):
+        # Função para enviar uma mensagem BYE para todos os vizinhos
+        print("Saindo...")
+        peer.desconectar_vizinhos()
+        sleep(2)
+        sys.exit(0)
 
     @classmethod
     def run(self):
@@ -89,11 +131,12 @@ class Main:
         main_instance = self(endereco, porta, vizinhos,
                              lista_chave_valor)
 
-        peer = Peer(main_instance.endereco, main_instance.porta,
+        peer = ServidorTCP(main_instance.endereco, main_instance.porta,
                     main_instance.vizinhos, main_instance.lista_chave_valor)
         peer.start()
 
-        print("""
+        while True:
+            print("""
 Escolha o comando
     [0] Listar vizinhos
     [1] HELLO
@@ -102,9 +145,7 @@ Escolha o comando
     [4] SEARCH (busca em profundidade)
     [5] Estatisticas
     [6] Alterar valor padrao de TTL
-    [9] Sair
-            """)
-        while True:
+    [9] Sair""")
             escolha = input("").split(" ")[0]
 
             if escolha == "0":
@@ -112,23 +153,24 @@ Escolha o comando
             elif escolha == "1":
                 main_instance.hello(peer)
             elif escolha == "2":
-                main_instance.listar_vizinhos()
+                main_instance.iniciar_flooding(peer)
             elif escolha == "3":
-                main_instance.listar_vizinhos()
+                main_instance.iniciar_random_walk(peer)
             elif escolha == "4":
-                main_instance.listar_vizinhos()
+                main_instance.iniciar_busca_profundidade(peer)
             elif escolha == "5":
-                main_instance.listar_vizinhos()
+                main_instance.estatisticas(peer)
             elif escolha == "6":
                 input_ttl = input("Digite o novo valor de TTL: ")
                 try:
-                    main_instance.ttl = int(input_ttl)
+                    main_instance.peer.servidor.ttl = int(input_ttl)
                 except ValueError:
                     print("TTL deve ser um número inteiro")
             elif escolha == "9":
-                break
+                main_instance.bye(peer)
             else:
                 print("Opção inválida")
+            sleep(1)
 
 
 if __name__ == "__main__":
--- servidor_tcp.py
+++ servidor_tcp.py
@@ -1,45 +1,215 @@
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
+from time import sleep
+from random import choice
 
 
 class ServidorTCP:
-    def __init__(self, endereco, porta):
+    def __init__(self, endereco, porta, vizinhos=None, lista_chave_valor=None):
+        # Inicializa o servidor com o endereço, porta, lista de vizinhos e lista de chave-valor.
         self.endereco = endereco
         self.porta = porta
+        self.vizinhos = vizinhos
+        self.vizinhos_conectados = []
+        self.lista_chave_valor = lista_chave_valor
         self.socket = None
+        self.mensagens_vistas_flooding = set()
+        self.mensagens_vistas_busca_profundidade = set()
+        self.ttl = 100
+        self.sequencia = 1
 
     def bind(self):
+        # Liga o servidor ao endereço e porta especificados.
         try:
             self.socket = socket(AF_INET, SOCK_STREAM)
             self.socket.bind((self.endereco, self.porta))
             self.socket.listen(1)
             print(f'Servidor criado: {self.endereco}:{self.porta}\n')
         except Exception as e:
-            print(f'Erro: {e}')
+            print(f'Erro Bind: {e}')
 
     def run(self):
+        # Inicia o servidor e aguarda conexões de clientes.
         while True:
             client_socket, client_address = self.socket.accept()
-            print(f'Conexão do cliente: {client_address}')
+            # print(f'Conexão do cliente: {client_address}')
             cliente_thread = Thread(target=self.handle_client, args=(
                 client_socket,))
             cliente_thread.start()
 
     def handle_client(self, client_socket):
+        # Lida com as mensagens recebidas dos clientes.
         while True:
             data = client_socket.recv(1024).decode()
             if not data:
                 break
-            print(f'Recebido: {data}')
+            print(f'Mensagem recebida: {data}')
             try:
                 self.trata_mensagem(data)
             except Exception as e:
-                print(f'Erro: {e}')
+                print(f'Erro Handle Client: {e}')
         client_socket.close()
         
+    def conectar_vizinhos(self):
+        # Envia uma mensagem HELLO para cada vizinho.
+        if not self.vizinhos:
+            return
+        for vizinho in self.vizinhos:
+            print(f'Tentando adicionar vizinho {vizinho}')
+            self.enviar_mensagem(vizinho, f"{self.endereco}:{self.porta} {self.sequencia} {self.ttl} HELLO")
+            
+    def desconectar_vizinhos(self):
+        # Envia uma mensagem BYE para cada vizinho.
+        if not self.vizinhos:
+            return
+        for vizinho in self.vizinhos:
+            self.enviar_mensagem(vizinho, f"{self.endereco}:{self.porta} {self.sequencia} {self.ttl} BYE")
+        
+    def enviar_mensagem(self, vizinho, mensagem):
+        # Envia uma mensagem para um vizinho específico.
+        try:
+            print(f'Encaminhando mensagem "{mensagem}" para {vizinho}')
+            cliente = socket(AF_INET, SOCK_STREAM)
+            cliente.connect((vizinho.split(":")[0], int(vizinho.split(":")[1])))
+            cliente.sendall(mensagem.encode())
+            print(f'\tEnvio feito com sucesso: {mensagem}')
+            cliente.close()
+        except Exception as e:
+            print(f'\tErro ao conectar! {e}')
+            
+    def processa_hello(self, mensagem):
+        # Adiciona o vizinho na lista de vizinhos conectados.
+        origin = mensagem.split(" ")[0]
+        if origin not in self.vizinhos_conectados:
+            self.vizinhos_conectados.append(origin)
+            print(f'\tAdicionando vizinho na tabela: {origin}')
+        else:
+            print(f'\tVizinho ja esta na tabela: {origin}')
+            
+    def processa_valor(self, mensagem):
+        # Processa a mensagem VAL e exibe a chave e o valor.
+        mensagem_split = mensagem.split(" ")
+        chave = mensagem_split[5]
+        valor = mensagem_split[6]
+        print(f'\tValor encontrada!\n\t\tchave: {chave} valor: {valor}')
+                    
+    def processa_flooding(self, mensagem):
+        # Pegar o endereco de origem, a sequencia, o ttl, a chave, o hop count da mensagem
+        mensagem_split = mensagem.split(" ")
+        endereco_origem = mensagem_split[0]
+        sequencia = mensagem_split[1]
+        ttl = int(mensagem_split[2])
+        chave = mensagem_split[6]
+        hop_count = int(mensagem_split[7])
+        # Verificar se a mensagem ja foi vista
+        if (endereco_origem, sequencia) in self.mensagens_vistas_flooding:
+            print(f'\tFlooding: Mensagem repetida! {mensagem}')
+            return
+        # Adicionar a mensagem na lista de mensagens vistas
+        self.mensagens_vistas_flooding.add((endereco_origem, sequencia))
+        # Verificar se a chave esta na lista chave valor
+        if chave in self.lista_chave_valor:
+            print(f'\tChave encontrada!')
+            self.enviar_mensagem(endereco_origem, f"{self.endereco}:{self.porta} {self.sequencia} {self.ttl} VAL FL {chave} {self.lista_chave_valor[chave]} {hop_count}")
+            return
+        # Decrementar TTL e verificar se ele é maior que 0
+        ttl -= 1
+        if ttl == 0:
+            print(f'\tTTL igual a zero, descartando mensagem')
+            return
+        # Incrementar hop count e enviar a mensagem para todos os vizinhos
+        for vizinho in self.vizinhos:
+            if vizinho == endereco_origem:
+                continue
+            self.enviar_mensagem(vizinho, f"{endereco_origem} {sequencia} {ttl} SEARCH FL {self.porta} {chave} {hop_count + 1}")
+            
+        
+    def processa_random_walk(self, mensagem):
+        # Pegar o endereco de origem, a sequencia, o ttl, a chave, o hop count da mensagem
+        mensagem_split = mensagem.split(" ")
+        endereco_origem = mensagem_split[0]
+        sequencia = mensagem_split[1]
+        ttl = int(mensagem_split[2])
+        chave = mensagem_split[6]
+        hop_count = int(mensagem_split[7])
+        # Verificar se a chave esta na lista chave valor
+        if chave in self.lista_chave_valor:
+            print(f'\tChave encontrada!')
+            self.enviar_mensagem(endereco_origem, f"{self.endereco}:{self.porta} {self.sequencia} {self.ttl} VAL RW {chave} {self.lista_chave_valor[chave]} {hop_count}")
+            return
+        # Decrementar TTL e verificar se ele é maior que 0
+        ttl -= 1
+        if ttl == 0:
+            print(f'\tTTL igual a zero, descartando mensagem')
+            return
+        # Incrementar hop count e enviar a mensagem para um vizinho aleatorio
+        vizinho_escolhido = choice(self.vizinhos)
+        self.enviar_mensagem(vizinho_escolhido, f"{endereco_origem} {sequencia} {ttl} SEARCH RW {self.porta} {chave} {hop_count + 1}")
+    
+    def processa_busca_profundidade(self, mensagem):
+        # Pegar o endereco de origem, a sequencia, o ttl, a chave, o hop count da mensagem
+        mensagem_split = mensagem.split(" ")
+        endereco_origem = mensagem_split[0]
+        sequencia = mensagem_split[1]
+        ttl = int(mensagem_split[2])
+        ultimo_vizinho = f'{self.endereco}:{mensagem_split[5]}'
+        chave = mensagem_split[6]
+        hop_count = int(mensagem_split[7])
+        vizinho_ativo = mensagem_split[0]
+        # Verificar se a chave esta na lista chave valor
+        if chave in self.lista_chave_valor:
+            print(f'\tChave encontrada!')
+            self.enviar_mensagem(endereco_origem, f"{self.endereco}:{self.porta} {self.sequencia} {self.ttl} VAL BP {chave} {self.lista_chave_valor[chave]} {hop_count}")
+            return
+        # Decrementar TTL e verificar se ele é maior que 0
+        ttl -= 1
+        if ttl == 0:
+            print(f'\tTTL igual a zero, descartando mensagem')
+            return
+        # Se for a primeira vez que a mensagem é vista, adicionar na lista de mensagens vistas
+        if (endereco_origem, sequencia) not in self.mensagens_vistas_busca_profundidade:
+            self.mensagens_vistas_busca_profundidade.add((endereco_origem, sequencia))
+            noh_mae = ultimo_vizinho
+            vizinhos_candidatos = self.vizinhos.copy()
+        # Remover o ultimo vizinho da lista de vizinhos candidatos
+        vizinhos_candidatos.remove(ultimo_vizinho)
+        # Condição de parada
+        if noh_mae == f'{self.endereco}:{self.porta}' and not vizinhos_candidatos == 0 and vizinho_ativo == ultimo_vizinho:
+            print(f'\tBP: Nao foi possivel localizar a chave {chave}')
+            return
+        
+        
+    
+    def processa_bye(self, mensagem):
+        # Remove o vizinho da lista de vizinhos conectados.
+        endereco_origem = mensagem.split(" ")[0]
+        print(f'\tRemovendo vizinho da tabela {endereco_origem}')
+        self.vizinhos_conectados.remove(endereco_origem)
+        
     def trata_mensagem(self, mensagem):
-        pass
+        # Trata a mensagem recebida.
+        mensagem_split = mensagem.split(" ")
+        if mensagem_split[3] == "HELLO":
+            self.processa_hello(mensagem)
+        elif mensagem_split[3] == "VAL":
+            self.processa_valor(mensagem)
+        elif mensagem_split[3] == "BYE":
+            self.processa_bye(mensagem)
+        elif mensagem_split[4] == "FL":
+            self.processa_flooding(mensagem)
+        elif mensagem_split[4] == "RW":
+            self.processa_random_walk(mensagem)
+        elif mensagem_split[4] == "BP":
+            self.processa_busca_profundidade(mensagem)
+        else:
+            print(f'Mensagem desconhecida: {mensagem}')
+        self.sequencia += 1
+            
 
     def start(self):
+        # Inicia o servidor e conecta os vizinhos.
         self.bind()
-        self.run()
+        servidor_thread = Thread(target=self.run)
+        servidor_thread.start()
+        sleep(0.5)
+        self.conectar_vizinhos()
